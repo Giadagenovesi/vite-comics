222a76d1938d1bb0a912dc2f2ba2edd7b7d7e107
Giadagenovesi <giada.g@outlook.it>
2023-04-13 17:06:21 +0200

    completed header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,108 @@
 <script>
 export default {
     name: "AppHeader",
-
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    current: "false"
+                },
+                {
+                    text: "COMICS",
+                    current: "true"
+                },
+                {
+                    text: "MOVIES",
+                    current: "false"
+                },
+                {
+                    text: "TV",
+                    current: "false"
+                },
+                {
+                    text: "GAMES",
+                    current: "false"
+                },
+                {
+                    text: "COLLECTIBLES",
+                    current: "false"
+                },
+                {
+                    text: "VIDEOS",
+                    current: "false"
+                },
+                {
+                    text: "FANS",
+                    current: "false"
+                },
+                {
+                    text: "NEWS",
+                    current: "false"
+                },
+                {
+                    text: "SHOP",
+                    current: "false"
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <template>
-    <h1> Header</h1>
+    
+        <div class="container">
+            <header>
+                <div class="logo">
+                    <img src="../assets/img/dc-logo.png" alt="dc logo">
+                </div>
+                <nav>
+                    <ul class="header-nav">
+                        <li v-for="link in links" > 
+                            <a href="">
+                                {{ link.text }}
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            
+        </div>
+    
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use "../style/partials/mixins" as *;
+header {
+    @include flex (row, space-between, center);
+    height: 70px;
+    margin: .8rem 0;
+
+    .logo img {
+        width: 60px;
+    }
+    .header-nav {
+        list-style: none;
+        @include flex (row, space-between, center);
+
+        li {
+            margin: 0 .6rem;
+            padding: 2.3rem 0;
+
+            a {
+                text-decoration: none;
+                color: black;
+                font-size: .8rem;
+            }
+            &.active,
+            &:hover {
+                border-bottom: 4px solid blue;
+            }
+        }
+
+
+    }
+
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,7 +5,21 @@ export default {
 </script>
 
 <template>
-    <h2> Main</h2>
+    <main>
+        <div class="container">
+            <h2> Main</h2>
+        </div>
+    </main>
+    
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+main {
+    background-color: black;
+
+    h2 {
+    color: white;
+}
+}
+
+</style>
